c81a203f96881fd42ed4b3fa50bb701b4bf20923
Woojin Chung <goglxych97@hufs.ac.kr>
2025-07-05 01:05:38 +0900

    Create blocks.py

---
+++ model/blocks.py
@@ -0,0 +1,181 @@
+# ------------------------------------------------------------------------------
+# Source: MONAI Project (https://github.com/Project-MONAI/MONAI)
+# File: monai/networks/nets/basic_unet.py
+# License: Apache License, Version 2.0
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is copied from the MONAI project.
+# ------------------------------------------------------------------------------
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import Optional
+
+import torch
+import torch.nn as nn
+
+from monai.networks.blocks import Convolution, UpSample
+from monai.networks.layers.factories import Conv, Pool
+from monai.utils import ensure_tuple_rep
+
+
+class TwoConv(nn.Sequential):
+    """two convolutions."""
+
+    def __init__(
+        self,
+        spatial_dims: int,
+        in_chns: int,
+        out_chns: int,
+        act: str | tuple,
+        norm: str | tuple,
+        bias: bool,
+        dropout: float | tuple = 0.0,
+    ):
+        """
+        Args:
+            spatial_dims: number of spatial dimensions.
+            in_chns: number of input channels.
+            out_chns: number of output channels.
+            act: activation type and arguments.
+            norm: feature normalization type and arguments.
+            bias: whether to have a bias term in convolution blocks.
+            dropout: dropout ratio. Defaults to no dropout.
+
+        """
+        super().__init__()
+
+        conv_0 = Convolution(spatial_dims, in_chns, out_chns, act=act, norm=norm, dropout=dropout, bias=bias, padding=1)
+        conv_1 = Convolution(
+            spatial_dims, out_chns, out_chns, act=act, norm=norm, dropout=dropout, bias=bias, padding=1
+        )
+        self.add_module("conv_0", conv_0)
+        self.add_module("conv_1", conv_1)
+
+
+class Down(nn.Sequential):
+    """maxpooling downsampling and two convolutions."""
+
+    def __init__(
+        self,
+        spatial_dims: int,
+        in_chns: int,
+        out_chns: int,
+        act: str | tuple,
+        norm: str | tuple,
+        bias: bool,
+        dropout: float | tuple = 0.0,
+    ):
+        """
+        Args:
+            spatial_dims: number of spatial dimensions.
+            in_chns: number of input channels.
+            out_chns: number of output channels.
+            act: activation type and arguments.
+            norm: feature normalization type and arguments.
+            bias: whether to have a bias term in convolution blocks.
+            dropout: dropout ratio. Defaults to no dropout.
+
+        """
+        super().__init__()
+        max_pooling = Pool["MAX", spatial_dims](kernel_size=2)
+        convs = TwoConv(spatial_dims, in_chns, out_chns, act, norm, bias, dropout)
+        self.add_module("max_pooling", max_pooling)
+        self.add_module("convs", convs)
+
+
+class UpCat(nn.Module):
+    """upsampling, concatenation with the encoder feature map, two convolutions"""
+
+    def __init__(
+        self,
+        spatial_dims: int,
+        in_chns: int,
+        cat_chns: int,
+        out_chns: int,
+        act: str | tuple,
+        norm: str | tuple,
+        bias: bool,
+        dropout: float | tuple = 0.0,
+        upsample: str = "deconv",
+        pre_conv: nn.Module | str | None = "default",
+        interp_mode: str = "linear",
+        align_corners: bool | None = True,
+        halves: bool = True,
+        is_pad: bool = True,
+    ):
+        """
+        Args:
+            spatial_dims: number of spatial dimensions.
+            in_chns: number of input channels to be upsampled.
+            cat_chns: number of channels from the encoder.
+            out_chns: number of output channels.
+            act: activation type and arguments.
+            norm: feature normalization type and arguments.
+            bias: whether to have a bias term in convolution blocks.
+            dropout: dropout ratio. Defaults to no dropout.
+            upsample: upsampling mode, available options are
+                ``"deconv"``, ``"pixelshuffle"``, ``"nontrainable"``.
+            pre_conv: a conv block applied before upsampling.
+                Only used in the "nontrainable" or "pixelshuffle" mode.
+            interp_mode: {``"nearest"``, ``"linear"``, ``"bilinear"``, ``"bicubic"``, ``"trilinear"``}
+                Only used in the "nontrainable" mode.
+            align_corners: set the align_corners parameter for upsample. Defaults to True.
+                Only used in the "nontrainable" mode.
+            halves: whether to halve the number of channels during upsampling.
+                This parameter does not work on ``nontrainable`` mode if ``pre_conv`` is `None`.
+            is_pad: whether to pad upsampling features to fit features from encoder. Defaults to True.
+
+        """
+        super().__init__()
+        if upsample == "nontrainable" and pre_conv is None:
+            up_chns = in_chns
+        else:
+            up_chns = in_chns // 2 if halves else in_chns
+        self.upsample = UpSample(
+            spatial_dims,
+            in_chns,
+            up_chns,
+            2,
+            mode=upsample,
+            pre_conv=pre_conv,
+            interp_mode=interp_mode,
+            align_corners=align_corners,
+        )
+        self.convs = TwoConv(spatial_dims, cat_chns + up_chns, out_chns, act, norm, bias, dropout)
+        self.is_pad = is_pad
+
+    def forward(self, x: torch.Tensor, x_e: Optional[torch.Tensor]):
+        """
+
+        Args:
+            x: features to be upsampled.
+            x_e: optional features from the encoder, if None, this branch is not in use.
+        """
+        x_0 = self.upsample(x)
+
+        if x_e is not None and torch.jit.isinstance(x_e, torch.Tensor):
+            if self.is_pad:
+                # handling spatial shapes due to the 2x maxpooling with odd edge lengths.
+                dimensions = len(x.shape) - 2
+                sp = [0] * (dimensions * 2)
+                for i in range(dimensions):
+                    if x_e.shape[-i - 1] != x_0.shape[-i - 1]:
+                        sp[i * 2 + 1] = 1
+                x_0 = torch.nn.functional.pad(x_0, sp, "replicate")
+            x = self.convs(torch.cat([x_e, x_0], dim=1))  # input channels: (cat_chns + up_chns)
+        else:
+            x = self.convs(x_0)
+
+        return x
